2f1f49fb928d771963b971c51d364a24dfa540c9
adezoguns <ogunleyeadeola7@gmail.com>
2021-09-25 15:15:33 +0100

    First local commit.

---
+++ main.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask import render_template, redirect, request
+
+
+app=Flask(__name__)
+
+
+@app.route('/')
+def home():
+	return render_template('index.html')
+
+@app.route('/products')
+def products():
+	return render_template('product.html')
+
+@app.route('/about')
+def about():
+	return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+	return render_template('contact.html')
+
+
+
+if __name__=="__main__":
+
+	app.run(debug=False)
