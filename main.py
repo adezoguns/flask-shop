11aab6876aa4244bc4198c85fbd1ea6a77f4496e
Ogunleye Adeola <adeola.ogunleye@atheneum-partners.com>
2022-07-03 20:16:02 +0100

    Change the address.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask import render_template, redirect, request
 from werkzeug.serving import run_simple
 
-#
+
 app=Flask(__name__)
 
 
@@ -26,4 +26,4 @@ def contact():
 
 if __name__=="__main__":
 
-	run_simple('192.168.1.107', 9000, app, use_reloader=False)
+	run_simple('127.0.0.1', 9000, app, use_reloader=False)
